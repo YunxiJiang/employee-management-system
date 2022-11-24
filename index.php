25ba7cd8ce954f94e0a8cc38caef73739abef761
YunxiJiang <matthewtkd9@gmail.com>
2022-11-24 11:32:07 +0000

    Complet the dashboard page. Desgin and create database. Successfuly show the information from database on dashboard with php.

---
+++ index.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>employee management system</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  </head>
+  <style>
+    body,
+    html {
+        height: 100%;
+        margin: 0;
+    }
+
+    .bg {
+        background-image: url(/resources/image/background.jpg);
+        height: 100%;
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+
+    }
+    </style>
+  <body>
+    <div class="bg border">
+        <div class="login-form-bg h-100">
+            
+                <div class="row justify-content-center h-100">
+                    <div class="col-xl-6">
+                        <div class="form-input-content">
+                            <div class="card login-form mt-5">
+                                <div class="card-body shadow">
+
+                                    <h2 class="text-center pb-4">Employee Management System</h2>
+                                    
+                                    <div class="mx-auto" style="width: 100px;">
+                                        <a href="" class="btn btn-primary btn-lg">Start</a>
+                                      </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+           
+        </div>
+    </div>
+      
+        
+    
+
+  </body>
+</html>
